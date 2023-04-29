ace8cb87a26ac7dfdab919b16060a3ae5de216f5
nader mohammed <nader.mohammed.20x@gmail.com>
2023-04-30 01:22:41 +0300

    befor merge

--- app/controllers/Orders.php
+++ app/controllers/Orders.php
@@ -2,6 +2,13 @@
 
 class Orders extends Controller
 {
+    public $userModel;
+
+    public function __construct()
+    {
+        $this->userModel = $this->model('User');
+    }
+
     public function index()
     {
         $data = [
@@ -15,8 +22,26 @@ class Orders extends Controller
         $data['user_last_orders'] = $db->resultSet();*/
         $db->query('SELECT * FROM product');
         $data['products'] = $db->resultSet();
-        $db->query('SELECT * FROM users WHERE role = "User"');
-        $data['users'] = $db->resultSet();
+
+        $data['users'] = $this->userModel->getUsersByRole('User');
+
+        return $this->view('orders/index', $data);
+    }
+
+    public function create()
+    {
+        $data = [
+            "products" => [],
+            "user_last_orders" => [],
+            "users" => []
+        ];
+        $db = new Database();
+        /*$db->query('SELECT * FROM orders WHERE user_id = :user_id ORDER BY id DESC LIMIT 5');
+        $db->bind(':user_id', $_SESSION['user_id']);
+        $data['user_last_orders'] = $db->resultSet();*/
+        $db->query('SELECT * FROM product');
+        $data['products'] = $db->resultSet();
+        $data['users'] = $this->userModel->getUsersByRole('User');
 
         return $this->view('orders/index', $data);
     }

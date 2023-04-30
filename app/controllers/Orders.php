b178538e1591b89270b57500e04279b8afe2d18b
nader mohammed <nader.mohammed.20x@gmail.com>
2023-05-01 02:59:53 +0300

    orders creation completed

--- app/controllers/Orders.php
+++ app/controllers/Orders.php
@@ -43,16 +43,49 @@ class Orders extends Controller
 
     public function add()
     {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            print_r($_POST);
-            $data = [
-                'user_id' => $_POST['user_id'],
-                'products' => $_POST['products'],
-                'room' => $_POST['room'],
-                'note' => $_POST['note'],
-            ];
-            $_POST['status'] = 'success';
-            $data = json_encode($_POST);
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_GET['data'])) {
+            $data = json_decode($_GET['data']);
+            if ($data->note) {
+                $data->note = htmlspecialchars($data->note);
+            }
+            /*
+            $data = json_decode(array_keys($_POST)[0] . '""}}')->data;
+            // search for any {} and the content of it in string with reges and display it
+            $matches = [];
+            preg_match_all('/\{[^}]+\}/', array_keys(array_values($_POST)[0])[0], $matches);
+            // convert match into array of objects with json_decode and map
+            $data->products = array_map(function ($match) {
+                return json_decode($match);
+            }, $matches[0]);
+//            print_r($data);
+//            die();
+//            $data = [
+//                'user_id' => $_POST['user_id'],
+//                'products' => $_POST['products'],
+//                'room' => $_POST['room'],
+//                'note' => $_POST['note'],
+//            ];
+            */
+            $products_ids = array_map(function ($product) {
+                return $product->id;
+            }, $data->products);
+            $products_data = $this->orderModel->getProductsPrices($products_ids);
+            $products = [];
+//            print_r($data->products);
+            foreach ($products_data as $product) {
+
+                $products[$product->id] = $product;
+            }
+
+            $data->price = 0;
+            foreach ($data->products as $key => $product) {
+                $data->products[$key]->price = $product->price = $products[$product->id]->price;
+                $data->price += $product->price * $product->quantity;
+            }
+            $data->id = $this->orderModel->addOrder($data);
+            $data->status = 'success';
+            flash('user_message', "Order #{$data->id} Created with Total invoice {$data->price}L.E", 'success');
+            $data = json_encode($data);
             die($data);
         }
     }
